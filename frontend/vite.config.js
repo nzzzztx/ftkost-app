f1498589b956f4229b674c432894943abcf0626c
nzzzztx <nathasy234@gmail.com>
2026-01-22 08:40:04 +0700

    nambah code di vite api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    proxy: {
+      '/storage': 'http://192.168.2.20:8000',
+      '/api': 'http://192.168.2.20:8000',
+    },
   },
 });
